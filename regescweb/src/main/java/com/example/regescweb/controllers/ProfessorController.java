b39e4451be617b559b4c297e429e67acb7cc4638
lucasg-c <lucasogjc@gmail.com>
2022-05-13 14:50:01 -0300

    Implementação da página de adicionar 'Novo Professor', incluindo, então, adequações no Index e no 'ProfessorController'. Adicionado novo status no enum 'StatusProfessor'.

--- regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
@@ -1,9 +1,11 @@
 package com.example.regescweb.controllers;
 
 import com.example.regescweb.models.Professor;
+import com.example.regescweb.models.StatusProfessor;
 import com.example.regescweb.repositories.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,6 +24,14 @@ public class ProfessorController {
         ModelAndView mv = new ModelAndView("professores/index");
         mv.addObject("professores", professores);
 
+        return mv;
+    }
+
+    @GetMapping("/professores/new")
+    public ModelAndView nnew()
+    {
+        ModelAndView mv = new ModelAndView("professores/new");
+        mv.addObject("statusProfessor", StatusProfessor.values());
         return mv;
     }
 }
--- regescweb/src/main/java/com/example/regescweb/models/StatusProfessor.java
+++ regescweb/src/main/java/com/example/regescweb/models/StatusProfessor.java
@@ -1,5 +1,5 @@
 package com.example.regescweb.models;
 
 public enum StatusProfessor {
-    ATIVO, INATIVO, APOSENTADO, AFASTADO
+    ATIVO, INATIVO, APOSENTADO, AFASTADO, DEMITIDO
 }

b21743c9069f0a6a600f5ed7692715db5d5cd914
lucasg-c <lucasogjc@gmail.com>
2022-05-15 19:11:44 -0300

    Recuperando objetos do DB por meio da página 'Show'.

--- regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/com/example/regescweb/controllers/ProfessorController.java
@@ -5,15 +5,19 @@ import com.example.regescweb.models.Professor;
 import com.example.regescweb.models.StatusProfessor;
 import com.example.regescweb.repositories.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.swing.text.html.Option;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -56,7 +60,30 @@ public class ProfessorController {
             Professor professor = requisicao.toProfessor();
             this.professorRepository.save(professor);
 
+            return new ModelAndView("redirect:/professores/" + professor.getId());
+        }
+    }
+
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id)
+    {
+        System.out.println("=-=-=-=-= ID: " + id + " =-=-=-=-");
+        Optional<Professor> optional = this.professorRepository.findById(id);
+        if (optional.isPresent())
+        {
+            Professor professor = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+
+            return mv;
+        }
+        else
+        {
+            System.out.println("=-=-=-=Não achou o objeto de ID " + id + " .=-=-=-=");
             return new ModelAndView("redirect:/professores");
         }
+//        ModelAndView mv = new ModelAndView("professores/show");
+//
+//        return mv;
     }
 }
